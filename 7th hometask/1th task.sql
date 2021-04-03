6668d699fd12d92d2b9563dec512eb0eaba8b368
SergeyZ06 <SergeyZ06@yandex.ru>
2021-04-03 12:09:14 +0300

    Added solution of the first task with 'JOIN'

--- 7th hometask/1th task.sql
+++ 7th hometask/1th task.sql
@@ -3,3 +3,9 @@
 SELECT *
 FROM users AS u
 WHERE EXISTS (SELECT 1 FROM orders AS o WHERE o.user_id = u.id);
+
+-- Решение с использованием JOIN
+
+SELECT DISTINCT u.*
+FROM users AS u
+	INNER JOIN orders AS o ON o.user_id = u.id;
