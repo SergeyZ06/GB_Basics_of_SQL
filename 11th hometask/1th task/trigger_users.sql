51c8f4540164447fc3178caca6ce3282ce9752b8
SergeyZ06 <SergeyZ06@yandex.ru>
2021-05-03 16:43:20 +0300

    There was a final fix triggers, now they work properly!

--- 11th hometask/1th task/trigger_users.sql
+++ 11th hometask/1th task/trigger_users.sql
@@ -1,9 +1,10 @@
 CREATE DEFINER=`SergeyZ06`@`%` TRIGGER `users_AFTER_INSERT` AFTER INSERT ON `users` FOR EACH ROW BEGIN
 	INSERT INTO `shop`.`logs` (`table`, `primary_key`, `name`, `datetime`)
-		VALUES ('users', 
-				(SELECT `AUTO_INCREMENT`
-				FROM  INFORMATION_SCHEMA.TABLES
-				WHERE TABLE_SCHEMA = 'shop'
-				AND   TABLE_NAME   = 'users'),
+		VALUES ('users',
+			NEW.id,
+--				(SELECT `AUTO_INCREMENT`
+--				FROM  INFORMATION_SCHEMA.TABLES
+--				WHERE TABLE_SCHEMA = 'shop'
+--				AND   TABLE_NAME   = 'users'),
 			NEW.name, NOW());
 END
